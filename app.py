5e056dd15eaf98cb24007cb41d0d0cfaf4c966cc
VarshaKumar09 <22d157@psgitech.ac.in>
2025-03-18 10:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello, World! This is a simple web app using Flask."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
